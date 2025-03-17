2c46d1e10e9fdd53dc7f810095e4a71bc01f526d
Tobias Bobb <Tobi.Bobb.TB@gmail.com>
2025-03-17 13:43:09 -0700

    Rewrite for Four H Bridges

--- Arduino/ESP_TESTING/ESP_TESTING.ino
+++ Arduino/ESP_TESTING/ESP_TESTING.ino
@@ -1,67 +1,95 @@
-//front left motor
+int motorA1_pin1 = 14;
+int motorA1_pin2 = 12;
 
-//drive blue and yellow
-int motor1pin1 = 12;
-int motor1pin2 = 14;
-int pwm1 = 13;
+int motorA2_pin1 = 27;
+int motorA2_pin2 = 26;
 
-//front right motor
-//drive red and green
-int pwm2 = 27;
-int motor2pin1 = 26;
-int motor2pin2 = 25;
+int motorA3_pin1 = 35;
+int motorA3_pin2 = 32;
 
-//front left motor
+int motorA4_pin1 = 5;
+int motorA4_pin2 = 18;
 
-//drive blue yellow
-int motor3pin1 = 17;
-int motor3pin2 = 5;
-int pwm3 = 16;
+int motorB1_pin1 = 2;
+int motorB1_pin2 = 4;
 
-//front right motor
-int pwm4 = 15;
-//drive red green
-int motor4pin1 = 4;
-int motor4pin2 = 2;
+int motorB2_pin1 = 16;
+int motorB2_pin2 = 17;
+
+int motorB3_pin1 = 21;
+int motorB3_pin2 = 19;
+
+int motorB4_pin1 = 23;
+int motorB4_pin2 = 22;
+
+int ena12 = 13;
+int ena34 = 25; 
+
+int motors[] = {
+motorA1_pin1,
+motorA1_pin2,
+motorA2_pin1,
+motorA2_pin2,
+motorA3_pin1,
+motorA3_pin2,
+motorA4_pin1,
+motorA4_pin2,
+motorB1_pin1,
+motorB1_pin2,
+motorB2_pin1,
+motorB2_pin2,
+motorB3_pin1,
+motorB3_pin2,
+motorB4_pin1,
+motorB4_pin2,
+};
+
+int motors_size = sizeof(motors)/sizeof(motors[0]);
+
+int motors_A [] = {
+  motorA1_pin1,
+  motorA1_pin2,
+  motorA2_pin1,
+  motorA2_pin2,
+  motorA3_pin1,
+  motorA3_pin2,
+  motorA4_pin1,
+  motorA4_pin2
+};
+
+int motors_B [] = {
+  motorB1_pin1,
+  motorB1_pin2,
+  motorB2_pin1,
+  motorB2_pin2,
+  motorB3_pin1,
+  motorB3_pin2,
+  motorB4_pin1,
+  motorB4_pin2,
+};
+
+int side_size = sizeof(motors_A)/sizeof(motors_A[0]);
+
+int enable_pins[] = {
+  ena12,
+  ena34
+};
 
 int motorSpeed = 125; //default
 
-void rightTurn();
-void leftTurn(); 
-void setSpeed(int pwm_freq );
-void stop(); 
-
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(motor1pin1, OUTPUT);
-  pinMode(motor1pin2, OUTPUT);
-  pinMode(motor2pin1,  OUTPUT);
-  pinMode(motor2pin2, OUTPUT);
 
-  pinMode(motor3pin1, OUTPUT);
-  pinMode(motor3pin2, OUTPUT);
-  pinMode(motor4pin1,  OUTPUT);
-  pinMode(motor4pin2, OUTPUT);
-
-
-  //(Optional)
-  pinMode(pwm1,  OUTPUT); 
-  pinMode(pwm2, OUTPUT);
-  pinMode(pwm3,  OUTPUT); 
-  pinMode(pwm4, OUTPUT);
+  for(int i = 0; i < motors_size; i++){
+    pinMode(motors[i], OUTPUT);
+  }
+  pinMode(ena12, OUTPUT);
+  pinMode(ena34, OUTPUT);
   
   Serial.begin(9600); 
-  
-  
-  //(Optional)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   //Controlling speed (0  = off and 255 = max speed):     
-  //(Optional)
-  
 
    if (Serial.available() > 0) {
     char command = Serial.read();
@@ -79,18 +107,15 @@ void loop() {
         break;
       case 'i':
         motorSpeed  += 10;
-        setSpeed(motorSpeed);
+        setSpeed();
         break;
       case 'd':
         motorSpeed -= 10;
-        setSpeed(motorSpeed);
+        setSpeed();
         break;
       case 's':
         stop();
         break;
-      // default:
-      //   // Test to see if the command is a number
-      //   stop();
       case 'f':
         forward();
         break; 
@@ -100,102 +125,122 @@ void loop() {
   
 }
 
-void setSpeed(int pwm_freq) {
+void setSpeed() {
   // Clamp the frequency to the range [0, 255]
-  pwm_freq = constrain(pwm_freq, 0, 255); 
+  if (motorSpeed > 255) {
+      motorSpeed = 255;
+  } else if (motorSpeed < 0) {
+      motorSpeed = 0;
+  }
   
   // Write to PWM pins
-  analogWrite(pwm1, pwm_freq); // ENA pin
-  analogWrite(pwm2, pwm_freq); // ENB pin
-  analogWrite(pwm3, pwm_freq); // ENA pin
-  analogWrite(pwm4, pwm_freq); // ENB pin
+  analogWrite(ena12, motorSpeed);
+  analogWrite(ena34, motorSpeed);
 }
 
 void backward(){
-  digitalWrite(motor1pin1,  LOW);
-  digitalWrite(motor1pin2, HIGH);
-
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-
-  digitalWrite(motor3pin1, LOW);
-  digitalWrite(motor3pin2, HIGH);
-
-  digitalWrite(motor4pin1, HIGH);
-  digitalWrite(motor4pin2, LOW);
+  // Needs to be tested
+  for(int i = 0; i < side_size; i+=2){
+    if(i%4==0){
+      digitalWrite(motors_A[i], LOW);
+      digitalWrite(motors_A[i+1], HIGH);
+      digitalWrite(motors_B[i], HIGH);
+      digitalWrite(motors_B[i+1], LOW);
+    }else{
+      digitalWrite(motors_A[i], HIGH);
+      digitalWrite(motors_A[i+1], LOW);
+      digitalWrite(motors_B[i], LOW);
+      digitalWrite(motors_B[i+1], HIGH);
+    }
+    
+  }
   delay(5);
 }
 
 
 void stop(){
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, LOW);
-
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, LOW);
-
-  digitalWrite(motor3pin1, LOW);
-  digitalWrite(motor3pin2, LOW);
-
-  digitalWrite(motor4pin1, LOW);
-  digitalWrite(motor4pin2, LOW);
-  delay(5);
-}
-
-void leftTurn(){
-  // motor goes up
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-
-  // motor goes down
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-
-  // motor goes down
-  digitalWrite(motor3pin1, HIGH);
-  digitalWrite(motor3pin2, LOW);
-
-  //motor goes up
-  digitalWrite(motor4pin1, HIGH);
-  digitalWrite(motor4pin2, LOW);
+  for(int i=0; i<16; i++){
+    digitalWrite(motors[i], LOW);
+  }
   delay(5);
 }
 
 void rightTurn(){
-  // motor goes down
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
+  for(int i=0; i<side_size; i++){
+    if(i%2==0){
+      digitalWrite(motors_A[i], HIGH);
+      digitalWrite(motors_B[i], LOW);
+    }else{
+      digitalWrite(motors_A[i], LOW);
+      digitalWrite(motors_B[i], HIGH);
+    }
+  }
+  // Manually writing to each motor
+  // digitalWrite(motorB1_pin1,LOW);
+  // digitalWrite(motorB1_pin2,HIGH);
+  // digitalWrite(motorB2_pin1,LOW);
+  // digitalWrite(motorB2_pin2,HIGH);
+  // digitalWrite(motorB3_pin1,LOW);
+  // digitalWrite(motorB3_pin2,HIGH);
+  // digitalWrite(motorB4_pin1,LOW);
+  // digitalWrite(motorB4_pin2,HIGH);
+  // digitalWrite(motorA1_pin1,HIGH);
+  // digitalWrite(motorA1_pin2,LOW);
+  // digitalWrite(motorA2_pin1,HIGH);
+  // digitalWrite(motorA2_pin2,LOW);
+  // digitalWrite(motorA3_pin1,HIGH);
+  // digitalWrite(motorA3_pin2,LOW);
+  // digitalWrite(motorA4_pin1,HIGH);
+  // digitalWrite(motorA4_pin2,LOW);
+  delay(5);
+}
 
-  // motor goes up
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-
-  // motor goes uo
-  digitalWrite(motor3pin1, LOW);
-  digitalWrite(motor3pin2, HIGH);
-
-  //motor goes down
-  digitalWrite(motor4pin1, LOW);
-  digitalWrite(motor4pin2, HIGH);
+void leftTurn(){
+    for(int i=0; i<side_size; i++){
+      if(i%2==0){
+        digitalWrite(motors_A[i], LOW);
+        digitalWrite(motors_B[i], HIGH);
+      }else{
+        digitalWrite(motors_A[i], HIGH);
+        digitalWrite(motors_B[i], LOW);
+      }
+    }
+  // Manually Writing to Motors
+  // digitalWrite(motorA1_pin1,LOW);
+  // digitalWrite(motorA1_pin2,HIGH);
+  // digitalWrite(motorA2_pin1,LOW);
+  // digitalWrite(motorA2_pin2,HIGH);
+  // digitalWrite(motorA3_pin1,LOW);
+  // digitalWrite(motorA3_pin2,HIGH);
+  // digitalWrite(motorA4_pin1,LOW);
+  // digitalWrite(motorA4_pin2,HIGH);
+  // digitalWrite(motorB1_pin1,HIGH);
+  // digitalWrite(motorB1_pin2,LOW);
+  // digitalWrite(motorB2_pin1,HIGH);
+  // digitalWrite(motorB2_pin2,LOW);
+  // digitalWrite(motorB3_pin1,HIGH);
+  // digitalWrite(motorB3_pin2,LOW);
+  // digitalWrite(motorB4_pin1,HIGH);
+  // digitalWrite(motorB4_pin2,LOW);
   delay(5);
 }
 
 void forward(){
-  // motor goes down
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-
-  // motor goes up
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-
-  // motor goes uo
-  digitalWrite(motor3pin1, HIGH);
-  digitalWrite(motor3pin2, LOW);
-
-  //motor goes down
-  digitalWrite(motor4pin1, LOW);
-  digitalWrite(motor4pin2, HIGH);
+  // Needs to be tested
+  for(int i = 0; i < side_size; i+=2){
+    if(i%4==0){
+      digitalWrite(motors_A[i], HIGH);
+      digitalWrite(motors_A[i+1], LOW);
+      digitalWrite(motors_B[i], LOW);
+      digitalWrite(motors_B[i+1], HIGH);
+    }else{
+      digitalWrite(motors_A[i], LOW);
+      digitalWrite(motors_A[i+1], HIGH);
+      digitalWrite(motors_B[i], HIGH);
+      digitalWrite(motors_B[i+1], LOW);
+    }
+    
+  }
   delay(5);
 }
 
